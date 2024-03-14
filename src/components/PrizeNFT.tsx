19bbaaf49095fa0c1e655a385c655b87a2453909
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2024-03-14 16:30:26 +0200

    Trying another adress

--- src/components/PrizeNFT.tsx
+++ src/components/PrizeNFT.tsx
@@ -1,53 +1,28 @@
 /* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
-import { NFT, ThirdwebNftMedia, useContract, useContractMetadata, useContractRead } from "@thirdweb-dev/react"
-import { RAFFLE_ADDRESS } from "../consts/addresses"
+import { ThirdwebNftMedia, useContract, useContractMetadata, useNFT } from "@thirdweb-dev/react"
+// import { RAFFLE_ADDRESS } from "../consts/addresses"
 import { Box, Card, Flex, Stack,Text,Heading } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
 
 export default function PrizeNFT() {
 
-    const {contract:raffleContract} = useContract(RAFFLE_ADDRESS);
-    const{data:prizeNFTContractAddress} = useContractRead(raffleContract,"nftAddress");
-    const{data:nftTokenId} = useContractRead(raffleContract,"nftId");
+    // const {contract:raffleContract} = useContract(RAFFLE_ADDRESS);
+    // const{data:prizeNFTContractAddress} = useContractRead(raffleContract,"nftAddress");
+    // const{data:nftTokenId} = useContractRead(raffleContract,"nftId");
 
-    const{contract:NftContract} = useContract(prizeNFTContractAddress);
+    const{contract:NftContract} = useContract("0x1526c6055c4bc8c8df2cfeb37f29a53dae6d79bb");
 
     const{data:prizeNftMetadata, isLoading:LoadingPrizeNft} = useContractMetadata(NftContract);
 
-    // const{data:nft, isLoading:LoadingNft} = useNFT(NftContract,nftTokenId);
-    // Define state for nft and loading state
-    const [nft, setNft] = useState<NFT>(); // Initial state is null
-    const [loadingNft, setLoadingNft] = useState(true); // Initial loading state is true
 
-    useEffect(() => {
-        // Define an async function to fetch NFT data
-        async function fetchNft() {
-            try {
-                const nftData = await NftContract?.erc721.get(nftTokenId);
-                if (nftData) {
-                setNft(nftData);
-            } else {
-                // Handle case when nftData is undefined
-                console.error('NFT data is undefined');
-            }
-            } catch (error) {
-                console.error('Error fetching NFT:', error);
-            } finally {
-                setLoadingNft(false); // Set loading state to false after fetching
-            }
-        }
+    const{data:nft, isLoading:LoadingNft} = useNFT(NftContract,36);
 
-        if (NftContract && nftTokenId) {
-            fetchNft();
-        }
-    }, [NftContract, nftTokenId]);
 
 
 
   return (
     <Card p={'15%'}>
         <Heading p={'2'} textAlign={"center"}>Prize NFT</Heading>
-        {!LoadingPrizeNft  &&!loadingNft && (
+        {!LoadingPrizeNft  && !LoadingNft && (
             <Stack  textAlign={'center'}>
                 <Flex justifyContent={"center"}>
                     <ThirdwebNftMedia metadata={nft?.metadata!} height="80%" width="80%"/>
