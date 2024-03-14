d41c616783b73632dfbcb202beec02ee168eab6b
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2024-03-14 15:08:01 +0200

    typo

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.VITE_TEMPLATE_CLIENT_ID': JSON.stringify(process.env.VITE_TEMPLATE_CLIENT_ID),
+    'VITE_TEMPLATE_CLIENT_ID': JSON.stringify(process.env.VITE_TEMPLATE_CLIENT_ID),
   },
 })
