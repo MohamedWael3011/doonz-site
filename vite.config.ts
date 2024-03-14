9101d0fab220d8d88c607b0560181926cf198030
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2024-03-14 15:10:01 +0200

    Vite Solution

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'VITE_TEMPLATE_CLIENT_ID': JSON.stringify(process.env.VITE_TEMPLATE_CLIENT_ID),
-  },
+export default defineConfig(({  mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    // vite config
+    define: {
+      VITE_TEMPLATE_CLIENT_ID: JSON.stringify(env.VITE_TEMPLATE_CLIENT_ID),
+    },
+  }
 })
