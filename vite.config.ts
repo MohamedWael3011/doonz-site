e9ce436171b3212143b42dd9654c5c53dbc7c1ae
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2024-03-14 15:01:13 +0200

    fixing config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-  return {
-    define: {
-      VITE_TEMPLATE_CLIENT_ID: process.env.VITE_TEMPLATE_CLIENT_ID,
-    },
-    plugins:[react()]
-  };
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    "process.env": process.env,
+    VITE_TEMPLATE_CLIENT_ID: process.env.VITE_TEMPLATE_CLIENT_ID,
+  },
 });
