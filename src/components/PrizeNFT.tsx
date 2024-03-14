1de0a9f40b67097fcae5f5a5d6892b43456d5375
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2024-03-14 16:24:30 +0200

    Tring manually loading

--- src/components/PrizeNFT.tsx
+++ src/components/PrizeNFT.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
-import { ThirdwebNftMedia, useContract, useContractMetadata, useContractRead, useNFT } from "@thirdweb-dev/react"
+import { NFT, ThirdwebNftMedia, useContract, useContractMetadata, useContractRead } from "@thirdweb-dev/react"
 import { RAFFLE_ADDRESS } from "../consts/addresses"
 import { Box, Card, Flex, Stack,Text,Heading } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 export default function PrizeNFT() {
 
@@ -13,14 +14,40 @@ export default function PrizeNFT() {
 
     const{data:prizeNftMetadata, isLoading:LoadingPrizeNft} = useContractMetadata(NftContract);
 
-    const{data:nft, isLoading:LoadingNft} = useNFT(NftContract,nftTokenId);
+    // const{data:nft, isLoading:LoadingNft} = useNFT(NftContract,nftTokenId);
+    // Define state for nft and loading state
+    const [nft, setNft] = useState<NFT>(); // Initial state is null
+    const [loadingNft, setLoadingNft] = useState(true); // Initial loading state is true
+
+    useEffect(() => {
+        // Define an async function to fetch NFT data
+        async function fetchNft() {
+            try {
+                const nftData = await NftContract?.erc721.get(nftTokenId);
+                if (nftData) {
+                setNft(nftData);
+            } else {
+                // Handle case when nftData is undefined
+                console.error('NFT data is undefined');
+            }
+            } catch (error) {
+                console.error('Error fetching NFT:', error);
+            } finally {
+                setLoadingNft(false); // Set loading state to false after fetching
+            }
+        }
+
+        if (NftContract && nftTokenId) {
+            fetchNft();
+        }
+    }, [NftContract, nftTokenId]);
 
 
 
   return (
     <Card p={'15%'}>
         <Heading p={'2'} textAlign={"center"}>Prize NFT</Heading>
-        {!LoadingPrizeNft  && !LoadingNft && (
+        {!LoadingPrizeNft  &&!loadingNft && (
             <Stack  textAlign={'center'}>
                 <Flex justifyContent={"center"}>
                     <ThirdwebNftMedia metadata={nft?.metadata!} height="80%" width="80%"/>
