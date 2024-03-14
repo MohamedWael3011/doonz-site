64ffb8a6888b1b8ffb5b2d4d5a04efcdcf35c3bb
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2024-03-14 16:32:26 +0200

    nvm not the issue

--- src/components/PrizeNFT.tsx
+++ src/components/PrizeNFT.tsx
@@ -1,20 +1,20 @@
 /* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
-import { ThirdwebNftMedia, useContract, useContractMetadata, useNFT } from "@thirdweb-dev/react"
-// import { RAFFLE_ADDRESS } from "../consts/addresses"
+import { ThirdwebNftMedia, useContract, useContractMetadata, useContractRead, useNFT } from "@thirdweb-dev/react"
+import { RAFFLE_ADDRESS } from "../consts/addresses"
 import { Box, Card, Flex, Stack,Text,Heading } from "@chakra-ui/react";
 
 export default function PrizeNFT() {
 
-    // const {contract:raffleContract} = useContract(RAFFLE_ADDRESS);
-    // const{data:prizeNFTContractAddress} = useContractRead(raffleContract,"nftAddress");
-    // const{data:nftTokenId} = useContractRead(raffleContract,"nftId");
+    const {contract:raffleContract} = useContract(RAFFLE_ADDRESS);
+    const{data:prizeNFTContractAddress} = useContractRead(raffleContract,"nftAddress");
+    const{data:nftTokenId} = useContractRead(raffleContract,"nftId");
 
-    const{contract:NftContract} = useContract("0x1526c6055c4bc8c8df2cfeb37f29a53dae6d79bb");
+    const{contract:NftContract} = useContract(prizeNFTContractAddress);
 
     const{data:prizeNftMetadata, isLoading:LoadingPrizeNft} = useContractMetadata(NftContract);
 
 
-    const{data:nft, isLoading:LoadingNft} = useNFT(NftContract,36);
+    const{data:nft, isLoading:LoadingNft} = useNFT(NftContract,nftTokenId);
 
 
 
@@ -22,7 +22,7 @@ export default function PrizeNFT() {
   return (
     <Card p={'15%'}>
         <Heading p={'2'} textAlign={"center"}>Prize NFT</Heading>
-        {!LoadingPrizeNft  && !LoadingNft && (
+        {!LoadingPrizeNft  && !LoadingNft (
             <Stack  textAlign={'center'}>
                 <Flex justifyContent={"center"}>
                     <ThirdwebNftMedia metadata={nft?.metadata!} height="80%" width="80%"/>
